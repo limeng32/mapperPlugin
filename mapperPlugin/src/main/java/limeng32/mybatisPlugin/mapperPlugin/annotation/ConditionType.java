35a98341d21bc4c3304b2e2a6d5ad7646231baeb
limeng32 <limeng32@live.cn>
2015-08-21 17:59:12 +0800

    Initial the project.

---
+++ mapperPlugin/src/main/java/limeng32/mybatisPlugin/mapperPlugin/annotation/ConditionType.java
@@ -0,0 +1,5 @@
+package limeng32.mybatisPlugin.mapperPlugin.annotation;
+
+public enum ConditionType {
+	Equal, Like, HeadLike, TailLike, GreaterThan, GreaterOrEqual, LessThan, LessOrEqual, NotEqual
+}
